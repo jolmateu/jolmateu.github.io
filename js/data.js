ccdd634b2d01396cbc6349a1ab7db77f5c3e1d0f
jolmateu <85537354+jolmateu@users.noreply.github.com>
2021-06-09 20:18:08 -0500

    Primary upload

---
+++ js/data.js
@@ -0,0 +1,6 @@
+let d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentdate").textContent = year
+
+var lastMod = document.lastModified;
+document.getElementById("lastmodified").textContent = lastMod
