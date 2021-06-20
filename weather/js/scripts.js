7f98c0c29b0ad036ef7a03c7bfe3a5c4a3fd15ca
jolmateu <85537354+jolmateu@users.noreply.github.com>
2021-06-19 22:43:57 -0500

    Upload date

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -2,3 +2,10 @@ function toggleMenu() {
     console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide")
 }
+
+let d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentdate").textContent = year
+
+var lastMod = document.lastModified;
+document.getElementById("lastmodified").textContent = lastMod
