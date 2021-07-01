df66d639fe002f959b973206ce640a578a9705c1
jolmateu <85537354+jolmateu@users.noreply.github.com>
2021-06-30 20:59:17 -0500

    Upload Gallery

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -11,4 +11,39 @@ var date = currentDate.getDate();
 var month = currentDate.getMonth();
 var year = currentDate.getFullYear();
                            
-document.getElementById("lastmodified").textContent = `${days[day]}, ${date} ${months[month]} ${year}`; 
+document.getElementById("lastmodified").textContent = `${days[day]}, ${date} ${months[month]} ${year}`;
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+          }
+      });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+
+  } else {
+      imagesToLoad.forEach((img) => {
+          loadImages(img);
+      });
+  }
