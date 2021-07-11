d34801b18581e0393e8a39765b1903872297bfef
jolmateu <85537354+jolmateu@users.noreply.github.com>
2021-07-10 23:49:56 -0500

    Preston 10

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -3,13 +3,14 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    document.getElementById('condition').textContent = jsObject.weather[0].description;
     document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('max-temp').textContent = jsObject.main.temp_max;
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('wind-speed').textContent = jsObject.wind.speed;
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
     const desc = jsObject.weather[0].description;  // note how we reference the weather array
     document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
     document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
     document.getElementById('icon').setAttribute('alt', desc);
-
   });
-
-  
